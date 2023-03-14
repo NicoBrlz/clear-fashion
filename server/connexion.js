e2b816c0c4777289e2ced44983b18bd9f1ced53b
Nicolas BERLIOZ <nberlioz@hotmail.fr>
2023-03-14 14:52:28 +0100

    step 4 up to 4/5 of the filters

--- server/connexion.js
+++ server/connexion.js
@@ -1,41 +1,87 @@
-const {MongoClient, Collection} = require('mongodb');
+const {MongoClient} = require('mongodb');
 const fs = require('fs');
+const { NONAME } = require('dns');
 
 const file = './products.json';
 
 const MONGODB_URI = 'mongodb+srv://Tacos:breakshop@api.0snggxh.mongodb.net/?retryWrites=true&writeConcern=majority';
 const MONGODB_DB_NAME = 'API';
 
+var db, client;
 
 async function connection(){
-    const client = await MongoClient.connect(MONGODB_URI,{'useNewUrlParser': true});
     try {
+        client = await MongoClient.connect(MONGODB_URI,{'useNewUrlParser': true});
         
-        const db = client.db(MONGODB_DB_NAME);
-    
-        //reding json file
-        const data = JSON.parse(fs.readFileSync(file));
-        console.log(data);
+        console.log('Database connected')
 
-        //insert the data in products collection
+        db = client.db(MONGODB_DB_NAME);
+        //collection = db.collection('products');
 
-        //console.log(await data.result);
-        const result = await db.collection('products').insertMany(data, {upsert: true});
-
-        console.log(`${result.insertedCount} documents insérés avec succès`);
+        return db;
+        
+        
     }
     catch (error) {
     console.error(error);
     } 
+
+    /*
     finally {
     client.close();
     }
-
-    
-
-
+    */
 }
 
-connection();
+async function insertion(){
+    const db = await connection();
+    //reading json file
+    const data = JSON.parse(fs.readFileSync(file));
+    console.log(data);
+
+    //insert the data in products collection
+    //console.log(await data);
+    const result = await db.collection('products').insertMany(data, {upsert: true});
+
+    console.log(`${result.insertedCount} documents insérés avec succès`);
+
+    client.close();
+    console.log('Database disconnected');
+}
+
+
+async function fliterProducts(brandFilter = null, less = null, price = false, date = false, weeks = false){
+    const db = await connection();
+    var query = {}
+    var result;
+    if(brandFilter != null){
+        query.brand = brandFilter;
+    }
+    if(less != null){
+        query.price = {$lt: less};
+    }
+
+    result = await db.collection('products').find(query);
+    
+
+    if(price==true){
+        result = result.sort({price: 1});
+    }
+    if(date==true){
+        result = result.sort({date: 1});
+    }
+
+    result = await result.toArray();
+
+    console.log('resultat 1', result);
+    console.log('Number of products: ', result.length);
+
+    await client.close();
+    console.log('Database disconnected');
+}
+
+fliterProducts(brandFilter = 'dedicated', less = 100.0, date = true);
+
+
 
 
